b8077339940ce2a1ec8833c5d7ce5d3f448868af
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-02-05 07:17:56 +0000

    Update hello.js and add even.js

--- hello.js
+++ hello.js
@@ -3,3 +3,14 @@ const sayHello = function () {
 }
 
 sayHello();
+
+const sayHelloToConsole = function(name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
